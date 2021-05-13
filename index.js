3a97b098975181aff048e7ae943d761293d8cea9
Vyacheslav <wotinowww@mail.ru>
2021-05-13 22:02:58 +0500

    Третий коммит

--- index.js
+++ index.js
@@ -3,3 +3,14 @@ const jwt = require('jsonwebtoken')
 const User = require('../models/User')
 const Category = require('../models/Category')
 //require('dotenv').config()
+
+const resolvers = {
+
+    Mutation: {
+        register: async (parent, {email, password}, context, info) => {
+
+            const candidate = await User.findOne({email: email})
+
+            if (candidate) {
+                return {email: email, message: 'Такой email уже занят. Попробуйте другой.'}
+            }
